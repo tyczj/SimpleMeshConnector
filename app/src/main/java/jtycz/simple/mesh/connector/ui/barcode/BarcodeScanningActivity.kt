1dccfbca790c563a22f3287537cc40d9a475b181
Jeff <tyczj359@gmail.com>
2019-01-03 21:32:16 -0500

    camera library

--- app/src/main/java/jtycz/simple/mesh/connector/ui/barcode/BarcodeScanningActivity.kt
+++ app/src/main/java/jtycz/simple/mesh/connector/ui/barcode/BarcodeScanningActivity.kt
@@ -1,285 +1,81 @@
 package jtycz.simple.mesh.connector.ui.barcode
 
-import android.Manifest
-import android.annotation.SuppressLint
 import android.app.Activity
-import android.content.Context
 import android.content.Intent
-import android.content.pm.PackageManager
-import android.graphics.ImageFormat
-import android.graphics.Matrix
-import android.graphics.RectF
-import android.graphics.SurfaceTexture
-import android.hardware.camera2.*
-import android.media.ImageReader
 import android.os.Bundle
-import android.os.Handler
-import android.os.HandlerThread
-import androidx.core.content.ContextCompat
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.ml.vision.FirebaseVision
 import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcode
 import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcodeDetector
 import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcodeDetectorOptions
-import android.util.SparseIntArray
-import android.view.*
-import androidx.annotation.NonNull
 import com.google.firebase.ml.vision.common.FirebaseVisionImage
 import com.google.firebase.ml.vision.common.FirebaseVisionImageMetadata
+import com.otaliastudios.cameraview.*
 import jtycz.simple.mesh.connector.R
 import kotlinx.android.synthetic.main.camera_view.*
-import java.util.concurrent.Semaphore
 
+/**
+ * Using a 3rd party camera library for using interfacing with the camera because the camera API is stupid complex
+ * and unnecessary to show for this example, you can find the library used here
+ * https://github.com/natario1/CameraView
+ */
+class BarcodeScanningActivity : AppCompatActivity(),FrameProcessor {
 
-class BarcodeScanningActivity : AppCompatActivity(),ImageReader.OnImageAvailableListener {
-
-    private val ORIENTATIONS = SparseIntArray()
     private lateinit var detector:FirebaseVisionBarcodeDetector
-    private lateinit var cameraManager:CameraManager
-
-    private var cameraDevice: CameraDevice? = null
-    private var captureSession: CameraCaptureSession? = null
-    private var imageReader:ImageReader? = null
-    private var backgroundThread: HandlerThread? = null
-    private var backgroundHandler: Handler? = null
-
-
-    private val sessionCallback = object : CameraCaptureSession.StateCallback() {
-        override fun onConfigured(cameraCaptureSession: CameraCaptureSession) {
-            // The camera is already closed
-            if (cameraDevice == null) {
-                return
-            }
-            // When the session is ready, we start capture.
-            captureSession = cameraCaptureSession
-            triggerImageCapture()
-        }
-
-        override fun onConfigureFailed(cameraCaptureSession: CameraCaptureSession) {
-//            Log.w(TAG, "Failed to configure camera")
-        }
-    }
-
-    /**
-     * [CameraDevice.StateCallback] is called when [CameraDevice] changes its state.
-     */
-    private val stateCallback = object : CameraDevice.StateCallback() {
-
-        override fun onOpened(@NonNull cameraDevice: CameraDevice) {
-            this@BarcodeScanningActivity.cameraDevice = cameraDevice
-            this@BarcodeScanningActivity.cameraDevice?.createCaptureSession(listOf<Surface>(imageReader!!.surface),sessionCallback,null)
-        }
-
-        override fun onDisconnected(@NonNull cameraDevice: CameraDevice) {
-            cameraDevice.close()
-        }
-
-        override fun onError(@NonNull cameraDevice: CameraDevice, i: Int) {
-            cameraDevice.close()
-        }
-
-        override fun onClosed(@NonNull cameraDevice: CameraDevice) {
-            this@BarcodeScanningActivity.cameraDevice = null
-        }
-    }
-
-    /**
-     * A [CameraCaptureSession.CaptureCallback] that handles events related to JPEG capture.
-     */
-    private val captureCallback = object : CameraCaptureSession.CaptureCallback() {
-
-        override fun onCaptureProgressed(session: CameraCaptureSession,request: CaptureRequest,partialResult: CaptureResult) {
-//            Log.d(TAG, "Partial result")
-        }
-
-        override fun onCaptureCompleted(session: CameraCaptureSession,request: CaptureRequest,result: TotalCaptureResult) {
-//            session.close()
-//            mCaptureSession = null
-//            Log.d(TAG, "CaptureSession closed")
-        }
-    }
-
-    /**
-     * [TextureView.SurfaceTextureListener] handles several lifecycle events on a
-     * [TextureView].
-     */
-    private val surfaceTextureListener = object : TextureView.SurfaceTextureListener {
-
-        override fun onSurfaceTextureAvailable(texture: SurfaceTexture, width: Int, height: Int) {
-            openCamera(width, height)
-        }
-
-        override fun onSurfaceTextureSizeChanged(texture: SurfaceTexture, width: Int, height: Int) {
-            configureTransform(width, height)
-        }
-
-        override fun onSurfaceTextureDestroyed(texture: SurfaceTexture) = true
-
-        override fun onSurfaceTextureUpdated(texture: SurfaceTexture) = Unit
-
-    }
-
-    private fun triggerImageCapture() {
-        try {
-            val captureBuilder = cameraDevice!!.createCaptureRequest(CameraDevice.TEMPLATE_STILL_CAPTURE)
-            captureBuilder.addTarget(imageReader!!.surface)
-            captureBuilder.set(CaptureRequest.CONTROL_AE_MODE, CaptureRequest.CONTROL_AE_MODE_ON)
-            captureSession!!.capture(captureBuilder.build(), captureCallback, null)
-        } catch (cae: CameraAccessException) {
-
-        }
-    }
 
     override fun onCreate(savedInstanceState:Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.camera_view)
-        ORIENTATIONS.append(Surface.ROTATION_0, 90)
-        ORIENTATIONS.append(Surface.ROTATION_90, 0)
-        ORIENTATIONS.append(Surface.ROTATION_180, 270)
-        ORIENTATIONS.append(Surface.ROTATION_270, 180)
 
         val options:FirebaseVisionBarcodeDetectorOptions = FirebaseVisionBarcodeDetectorOptions.Builder()
-            .setBarcodeFormats(FirebaseVisionBarcode.FORMAT_QR_CODE)
+            .setBarcodeFormats(FirebaseVisionBarcode.FORMAT_DATA_MATRIX)
             .build()
 
         detector = FirebaseVision.getInstance().getVisionBarcodeDetector(options)
+
+        camera.setLifecycleOwner(this)
+        camera.addFrameProcessor(this)
+        camera.sessionType = SessionType.PICTURE
+        camera.facing = Facing.BACK
+        camera.flash = Flash.OFF
     }
 
     override fun onResume() {
         super.onResume()
-
-        if(textureView.isAvailable){
-
-        }else{
-            textureView.surfaceTextureListener = surfaceTextureListener
-        }
+        camera.start()
     }
 
-    override fun onImageAvailable(reader: ImageReader?) {
-        reader?.acquireNextImage().use {
-            if (it != null){
-                val image = FirebaseVisionImage.fromMediaImage(it,getRotationCompensation(cameraDevice!!.id,this,this))
-
-                detector.detectInImage(image)
-                    .addOnSuccessListener { barcodes ->
-                        for (barcode in barcodes){
-                            val rawValue = barcode.rawValue
-                            val intent = Intent()
-                            intent.putExtra("barcode",rawValue)
-                            setResult(Activity.RESULT_OK,intent)
-                            finish()
-                        }
-                    }
-                    .addOnFailureListener{}
-            }
-        }
+    override fun onPause() {
+        super.onPause()
+        camera.stop()
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        closeCamera()
+        camera.destroy()
     }
 
-    /**
-     * Opens the camera specified by [Camera2BasicFragment.cameraId].
-     */
-    private fun openCamera(width: Int, height: Int) {
-        backgroundThread = HandlerThread("BackgroundThread")
-        backgroundThread?.start()
-        backgroundHandler = Handler(backgroundThread?.looper)
-        backgroundHandler?.post(initializeOnBackground)
-    }
+    override fun process(frame: Frame) {
+        val metadata = FirebaseVisionImageMetadata.Builder()
+            .setWidth(480)
+            .setHeight(360)
+            .setFormat(FirebaseVisionImageMetadata.IMAGE_FORMAT_NV21)
+            .setRotation(FirebaseVisionImageMetadata.ROTATION_0) // always assuming portrait
+            .build()
 
-    /**
-     * Closes the current [CameraDevice].
-     */
-    private fun closeCamera() {
-        captureSession?.close()
-        captureSession = null
-        cameraDevice?.close()
-        cameraDevice = null
-        imageReader?.close()
-        imageReader = null
-    }
+        val image = FirebaseVisionImage.fromByteArray(frame.data, metadata)
 
-    @SuppressLint("MissingPermission")
-    private val initializeOnBackground = Runnable {
-        cameraManager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
-
-        imageReader = ImageReader.newInstance(480, 360, ImageFormat.YUV_420_888, 1)
-        imageReader?.setOnImageAvailableListener(this, backgroundHandler)
-
-        try {
-            val camIds = cameraManager.cameraIdList
-
-            if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED){
-                cameraManager.openCamera(camIds[0], stateCallback, backgroundHandler)
+        val result = detector.detectInImage(image)
+            .addOnSuccessListener { barcodes ->
+                val barcode = barcodes.first()
+                val intent = Intent()
+                intent.putExtra("barcode",barcode.rawValue)
+                setResult(Activity.RESULT_OK)
+                finish()
             }
-        } catch (e: CameraAccessException) {
+            .addOnFailureListener {
 
-        }
-    }
-
-    /**
-     * Configures the necessary [android.graphics.Matrix] transformation to `textureView`.
-     * This method should be called after the camera preview size is determined in
-     * setUpCameraOutputs and also the size of `textureView` is fixed.
-     *
-     * @param viewWidth  The width of `textureView`
-     * @param viewHeight The height of `textureView`
-     */
-    private fun configureTransform(viewWidth: Int, viewHeight: Int) {
-        val rotation = this.windowManager.defaultDisplay.rotation
-        val matrix = Matrix()
-        val viewRect = RectF(0f, 0f, viewWidth.toFloat(), viewHeight.toFloat())
-        val bufferRect = RectF(0f, 0f, previewSize.height.toFloat(), previewSize.width.toFloat())
-        val centerX = viewRect.centerX()
-        val centerY = viewRect.centerY()
-
-        if (Surface.ROTATION_90 == rotation || Surface.ROTATION_270 == rotation) {
-            bufferRect.offset(centerX - bufferRect.centerX(), centerY - bufferRect.centerY())
-            val scale = Math.max(
-                viewHeight.toFloat() / previewSize.height,
-                viewWidth.toFloat() / previewSize.width)
-            with(matrix) {
-                setRectToRect(viewRect, bufferRect, Matrix.ScaleToFit.FILL)
-                postScale(scale, scale, centerX, centerY)
-                postRotate((90 * (rotation - 2)).toFloat(), centerX, centerY)
             }
-        } else if (Surface.ROTATION_180 == rotation) {
-            matrix.postRotate(180f, centerX, centerY)
-        }
-        textureView.setTransform(matrix)
     }
 
-    private fun getRotationCompensation(cameraId: String, activity: Activity, context: Context): Int {
-        // Get the device's current rotation relative to its "native" orientation.
-        // Then, from the ORIENTATIONS table, look up the angle the image must be
-        // rotated to compensate for the device's rotation.
-        val deviceRotation = activity.windowManager.defaultDisplay.rotation
-        var rotationCompensation = ORIENTATIONS.get(deviceRotation)
-
-        // On most devices, the sensor orientation is 90 degrees, but for some
-        // devices it is 270 degrees. For devices with a sensor orientation of
-        // 270, rotate the image an additional 180 ((270 + 270) % 360) degrees.
-        val cameraManager = context.getSystemService(CAMERA_SERVICE) as CameraManager
-        val sensorOrientation = cameraManager
-            .getCameraCharacteristics(cameraId)
-            .get(CameraCharacteristics.SENSOR_ORIENTATION)!!
-        rotationCompensation = (rotationCompensation + sensorOrientation + 270) % 360
-
-        // Return the corresponding FirebaseVisionImageMetadata rotation value.
-        val result: Int
-
-        when (rotationCompensation) {
-            0 -> result = FirebaseVisionImageMetadata.ROTATION_0
-            90 -> result = FirebaseVisionImageMetadata.ROTATION_90
-            180 -> result = FirebaseVisionImageMetadata.ROTATION_180
-            270 -> result = FirebaseVisionImageMetadata.ROTATION_270
-            else -> {
-                result = FirebaseVisionImageMetadata.ROTATION_0
-            }
-        }
-        return result
-    }
 }
